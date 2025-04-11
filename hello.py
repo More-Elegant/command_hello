368c896c62b3eeedca7dfacb56cfdd40fc601caa
More-Elegant <105899672+More-Elegant@users.noreply.github.com>
2025-04-11 09:54:11 +0900

    github for git pull

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 //for command line git tutorial
 //local repository modification
-//for commed line git tutorial
 //gihub modification
 
 print("Hello World")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
